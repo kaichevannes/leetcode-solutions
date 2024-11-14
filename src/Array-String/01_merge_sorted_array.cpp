0c77f4eb58dc3cf2e1b7680abd89cb2c821e35f2
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-14 09:58:08 +0000

    feat: solution to Array/String problem 1

--- src/Array-String/01_merge_sorted_array.cpp
+++ src/Array-String/01_merge_sorted_array.cpp
@@ -1,6 +1,29 @@
 #include "01_merge_sorted_array.h"
 
-void MergeSortedArray::merge(std::vector<int>& nums1, int m,
-                             std::vector<int>& nums2, int n) {
-    nums2.push_back(1);
+void MergeSortedArray::merge(std::vector<int> &nums1, int m,
+                             std::vector<int> &nums2, int n) {
+  std::vector<int> output;
+  int i = 0; // Pointer to next unsorted element in nums1
+  int j = 0; // Pointer to next unsorted element in nums2
+
+  auto moreElementsToMerge = [&] { return i + j < m + n; };
+  auto lowestElementInNums1 = [&] { return nums1[i] < nums2[j]; };
+  auto nums1HasElements = [&] { return i < m; };
+  auto nums2HasElements = [&] { return j < n; };
+
+  while (moreElementsToMerge()) {
+    if (nums1HasElements() && nums2HasElements()) {
+      if (lowestElementInNums1()) {
+        output.push_back(nums1[i++]);
+      } else {
+        output.push_back(nums2[j++]);
+      }
+    } else if (nums1HasElements()) {
+      output.push_back(nums1[i++]);
+    } else if (nums2HasElements()) {
+      output.push_back(nums2[j++]);
+    }
+  }
+
+  nums1 = output;
 };
--- tests/Array-String/01_merge_sorted_array_test.cpp
+++ tests/Array-String/01_merge_sorted_array_test.cpp
@@ -4,17 +4,53 @@
 class MergeSortedArrayTest : public testing::Test {
 protected:
   MergeSortedArray mergeSortedArray;
+  std::vector<int> nums1;
+  std::vector<int> nums2;
 
-  void callMerge(std::vector<int>& nums1, std::vector<int>& nums2) {
+  void callMerge(std::vector<int> &nums1, std::vector<int> &nums2) {
     mergeSortedArray.merge(nums1, nums1.size(), nums2, nums2.size());
   }
 };
 
-TEST_F(MergeSortedArrayTest, setup) {
-  std::vector<int> nums1;
-  std::vector<int> nums2;
+TEST_F(MergeSortedArrayTest, SingleElementInNums1) {
+  nums1.push_back(1);
 
   callMerge(nums1, nums2);
 
-  EXPECT_EQ(nums2, std::vector<int>{1});
+  EXPECT_EQ(nums1, std::vector<int>{1});
+}
+
+TEST_F(MergeSortedArrayTest, SingleElementInNums2) {
+  nums2.push_back(1);
+
+  callMerge(nums1, nums2);
+
+  EXPECT_EQ(nums1, std::vector<int>{1});
+}
+
+TEST_F(MergeSortedArrayTest, Nums1FollowedByNums2) {
+  nums1 = {1, 2, 3};
+  nums2 = {4, 5, 6};
+
+  callMerge(nums1, nums2);
+
+  EXPECT_EQ(nums1, std::vector<int>({1, 2, 3, 4, 5, 6}));
+}
+
+TEST_F(MergeSortedArrayTest, Nums2FollowedByNums1) {
+  nums1 = {4, 5, 6};
+  nums2 = {1, 2, 3};
+
+  callMerge(nums1, nums2);
+
+  EXPECT_EQ(nums1, std::vector<int>({1, 2, 3, 4, 5, 6}));
+}
+
+TEST_F(MergeSortedArrayTest, NumsInterleaved) {
+  nums1 = {1, 3, 5};
+  nums2 = {2, 4, 6};
+
+  callMerge(nums1, nums2);
+
+  EXPECT_EQ(nums1, std::vector<int>({1, 2, 3, 4, 5, 6}));
 }
