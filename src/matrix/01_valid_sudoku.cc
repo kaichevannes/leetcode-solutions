aa066f380a1bcbf7226f4c6373375948e3a29482
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-16 19:33:38 +0000

    refactor: change names, remove magic literal

--- src/matrix/01_valid_sudoku.cc
+++ src/matrix/01_valid_sudoku.cc
@@ -1,59 +1,63 @@
 #include "01_valid_sudoku.h"
+#include <cassert>
 #include <unordered_set>
+
+constexpr int GRID_SIZE = 9;
+constexpr int BOX_SIZE = 3;
 
 struct Cell {
   int row, col;
 };
 
-class SeenIn {
+class SudokuRuleChecker {
 public:
-  SeenIn(std::vector<std::vector<char>> &existingBoard)
+  SudokuRuleChecker(std::vector<std::vector<char>> &existingBoard)
       : board(existingBoard) {}
 
-  void insert(Cell cell) {
-    row[cell.row].insert(number(cell));
-    col[cell.col].insert(number(cell));
-    box[boxIndex(cell)].insert(number(cell));
+  void addToValidationState(Cell cell) {
+    seenInRow[cell.row].insert(number(cell));
+    seenInCol[cell.col].insert(number(cell));
+    seenInBox[boxIndex(cell)].insert(number(cell));
   }
 
-  bool exists(Cell cell) {
-    return !(row[cell.row].count(number(cell)) ||
-             col[cell.col].count(number(cell)) ||
-             box[boxIndex(cell)].count(number(cell)));
+  bool isAllowed(Cell cell) const {
+    return !(seenInRow[cell.row].count(number(cell)) ||
+             seenInCol[cell.col].count(number(cell)) ||
+             seenInBox[boxIndex(cell)].count(number(cell)));
   }
 
 private:
-  std::unordered_set<char> row[9];
-  std::unordered_set<char> col[9];
-  std::unordered_set<char> box[9];
+  std::unordered_set<char> seenInRow[GRID_SIZE];
+  std::unordered_set<char> seenInCol[GRID_SIZE];
+  std::unordered_set<char> seenInBox[GRID_SIZE];
   std::vector<std::vector<char>> board;
 
-  int number(Cell cell) { return board[cell.row][cell.col]; }
+  int number(Cell cell) const { return board[cell.row][cell.col]; }
 
-  int boxIndex(Cell cell) {
-    int threeRowBlock = (cell.row / 3);
-    int threeRowCol = (cell.col / 3);
-    int boxesPerRow = 3;
-    return threeRowBlock * boxesPerRow + threeRowCol;
+  int boxIndex(Cell cell) const {
+    int threeRowBlockIndex = (cell.row / BOX_SIZE);
+    int threeRowColIndex = (cell.col / BOX_SIZE);
+    int boxesPerRow = BOX_SIZE;
+    return threeRowBlockIndex * boxesPerRow + threeRowColIndex;
   }
 };
 
 bool ValidSudoku::isValidSudoku(std::vector<std::vector<char>> &board) {
-  SeenIn seenIn(board);
+  assert(board.size() == GRID_SIZE && board[0].size() == GRID_SIZE);
+
+  SudokuRuleChecker sudokuRuleChecker(board);
 
   auto emptyCell = [&](Cell cell) { return board[cell.row][cell.col] == '.'; };
 
   for (int row = 0; row < board.size(); row++) {
     for (int col = 0; col < board.size(); col++) {
-      if (emptyCell({row, col})) {
+      if (emptyCell(Cell{row, col})) {
         continue;
       }
-
-      if (!seenIn.exists({row, col})) {
+      if (!sudokuRuleChecker.isAllowed(Cell{row, col})) {
         return false;
       }
-
-      seenIn.insert({row, col});
+      sudokuRuleChecker.addToValidationState(Cell{row, col});
     }
   }
   return true;
