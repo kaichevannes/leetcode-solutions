541816216009bbe6cf2228d0cac8b511ae8a504e
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-16 16:59:57 +0000

    refactor: introduce parameter object (cell)

--- src/matrix/01_valid_sudoku.cc
+++ src/matrix/01_valid_sudoku.cc
@@ -1,64 +1,63 @@
 #include "01_valid_sudoku.h"
 #include <unordered_set>
 
-class SudokuValidator {
-private:
-  std::vector<std::vector<char>> board;
-  std::unordered_set<char> seenInRow[9], seenInCol[9], seenInBox[9];
-
-public:
-  SudokuValidator(std::vector<std::vector<char>> &existingBoard)
-      : board(existingBoard) {}
-
-  bool validSquare(int row, int col) {
-    char number = board[row][col];
-
-    if (emptySquare(number))
-      return true;
-
-    if (!isValidNumberPlacement(number, row, col)) {
-      return false;
-    }
-
-    seen(number, row, col);
-    return true;
-  }
-
-private:
-  bool emptySquare(char squareValue) { return squareValue == '.'; }
-
-  int boxIndex(int row, int col) {
-    int threeRowBlock = (row / 3);
-    int threeRowCol = (col / 3);
-    int boxesPerRow = 3;
-    return threeRowBlock * boxesPerRow + threeRowCol;
-  }
-
-  bool isValidNumberPlacement(char number, int row, int col) {
-    if (seenInRow[row].count(number) || seenInCol[col].count(number) ||
-        seenInBox[boxIndex(row, col)].count(number)) {
-      return false;
-    }
-    return true;
-  }
-
-  void seen(char number, int row, int col) {
-    seenInRow[row].insert(number);
-    seenInCol[col].insert(number);
-    seenInBox[boxIndex(row, col)].insert(number);
-  }
+struct SeenIn {
+  std::unordered_set<char> row[9];
+  std::unordered_set<char> col[9];
+  std::unordered_set<char> box[9];
 };
 
+struct Cell {
+  int row, col;
+};
+
+int boxIndex(int row, int col) {
+  int threeRowBlock = (row / 3);
+  int threeRowCol = (col / 3);
+  int boxesPerRow = 3;
+  return threeRowBlock * boxesPerRow + threeRowCol;
+}
+
+void seen(char number, int row, int col, SeenIn &seenIn) {
+  seenIn.row[row].insert(number);
+  seenIn.col[col].insert(number);
+  seenIn.box[boxIndex(row, col)].insert(number);
+}
+
+bool emptySquare(char squareValue) { return squareValue == '.'; }
+
+bool isValidNumberPlacement(char number, int row, int col, SeenIn &seenIn) {
+  if (seenIn.row[row].count(number) || seenIn.col[col].count(number) ||
+      seenIn.box[boxIndex(row, col)].count(number)) {
+    return false;
+  }
+  return true;
+}
+
+bool validCell(Cell cell, std::vector<std::vector<char>> &board,
+                 SeenIn &seenIn) {
+  char number = board[cell.row][cell.col];
+
+  if (emptySquare(number))
+    return true;
+
+  if (!isValidNumberPlacement(number, cell.row, cell.col, seenIn)) {
+    return false;
+  }
+
+  seen(number, cell.row, cell.col, seenIn);
+  return true;
+}
+
 bool ValidSudoku::isValidSudoku(std::vector<std::vector<char>> &board) {
-  SudokuValidator validator(board);
+  SeenIn seenIn;
 
   for (int row = 0; row < board.size(); row++) {
     for (int col = 0; col < board.size(); col++) {
-      if (!validator.validSquare(row, col)) {
+      if (!validCell(Cell{row, col}, board, seenIn)) {
         return false;
       }
     }
   }
-
   return true;
 }
