34140304e14747c56b4e2cd170f4b8d81a82ba77
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-16 18:48:50 +0000

    refactor: inline everything

--- src/matrix/01_valid_sudoku.cc
+++ src/matrix/01_valid_sudoku.cc
@@ -5,60 +5,55 @@ struct Cell {
   int row, col;
 };
 
-int boxIndex(Cell cell) {
-  int threeRowBlock = (cell.row / 3);
-  int threeRowCol = (cell.col / 3);
-  int boxesPerRow = 3;
-  return threeRowBlock * boxesPerRow + threeRowCol;
-}
-
 class SeenIn {
 public:
-  void insert(int number, Cell cell) {
-    row[cell.row].insert(number);
-    col[cell.col].insert(number);
-    box[boxIndex(cell)].insert(number);
+  SeenIn(std::vector<std::vector<char>> &existingBoard)
+      : board(existingBoard) {}
+
+  void insert(Cell cell) {
+    row[cell.row].insert(number(cell));
+    col[cell.col].insert(number(cell));
+    box[boxIndex(cell)].insert(number(cell));
   }
 
-  bool exists(int number, Cell cell) {
-    return !(row[cell.row].count(number) ||
-             col[cell.col].count(number) ||
-             box[boxIndex(cell)].count(number));
+  bool exists(Cell cell) {
+    return !(row[cell.row].count(number(cell)) ||
+             col[cell.col].count(number(cell)) ||
+             box[boxIndex(cell)].count(number(cell)));
   }
 
 private:
   std::unordered_set<char> row[9];
   std::unordered_set<char> col[9];
   std::unordered_set<char> box[9];
+  std::vector<std::vector<char>> board;
+
+  int number(Cell cell) { return board[cell.row][cell.col]; }
+
+  int boxIndex(Cell cell) {
+    int threeRowBlock = (cell.row / 3);
+    int threeRowCol = (cell.col / 3);
+    int boxesPerRow = 3;
+    return threeRowBlock * boxesPerRow + threeRowCol;
+  }
 };
 
 bool emptyCell(Cell cell, std::vector<std::vector<char>> &board) {
   return board[cell.row][cell.col] == '.';
 }
 
-bool validCell(Cell cell, std::vector<std::vector<char>> &board,
-               SeenIn &seenIn) {
-  char number = board[cell.row][cell.col];
-
-  if (seenIn.exists(number, cell)) {
-    seenIn.insert(number, cell);
-    return true;
-  }
-
-  return false;
-}
-
 bool ValidSudoku::isValidSudoku(std::vector<std::vector<char>> &board) {
-  SeenIn seenIn;
+  SeenIn seenIn(board);
 
   for (int row = 0; row < board.size(); row++) {
     for (int col = 0; col < board.size(); col++) {
-      if (emptyCell(Cell{row, col}, board)) {
+      if (emptyCell({row, col}, board)) {
         continue;
       }
-      if (!validCell(Cell{row, col}, board, seenIn)) {
+      if (!seenIn.exists({row, col})) {
         return false;
       }
+      seenIn.insert({row, col});
     }
   }
   return true;
