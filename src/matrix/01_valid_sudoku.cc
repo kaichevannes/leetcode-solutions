47d49c5026776e21ce1dfdb34311fcf45cf389b1
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-16 16:20:40 +0000

    refactor: extract class (SudokuValidator)

--- src/matrix/01_valid_sudoku.cc
+++ src/matrix/01_valid_sudoku.cc
@@ -1,24 +1,62 @@
 #include "01_valid_sudoku.h"
-#include <set>
+#include <unordered_set>
+
+class SudokuValidator {
+private:
+  std::vector<std::vector<char>> board;
+  std::unordered_set<char> seenInRow[9], seenInCol[9], seenInBox[9];
+
+public:
+  SudokuValidator(std::vector<std::vector<char>> &existingBoard)
+      : board(existingBoard) {}
+
+  bool validSquare(int row, int col) {
+    char number = board[row][col];
+
+    if (emptySquare(number))
+      return true;
+
+    if (!isValidNumberPlacement(number, row, col)) {
+      return false;
+    }
+
+    seen(number, row, col);
+    return true;
+  }
+
+private:
+  bool emptySquare(char squareValue) { return squareValue == '.'; }
+
+  int boxIndex(int row, int col) {
+    int threeRowBlock = (row / 3);
+    int threeRowCol = (col / 3);
+    int boxesPerRow = 3;
+    return threeRowBlock * boxesPerRow + threeRowCol;
+  }
+
+  bool isValidNumberPlacement(char number, int row, int col) {
+    if (seenInRow[row].count(number) || seenInCol[col].count(number) ||
+        seenInBox[boxIndex(row, col)].count(number)) {
+      return false;
+    }
+    return true;
+  }
+
+  void seen(char number, int row, int col) {
+    seenInRow[row].insert(number);
+    seenInCol[col].insert(number);
+    seenInBox[boxIndex(row, col)].insert(number);
+  }
+};
 
 bool ValidSudoku::isValidSudoku(std::vector<std::vector<char>> &board) {
-  std::set<char> seenInRow[9], seenInCol[9], seenInBox[9];
+  SudokuValidator validator(board);
 
   for (int row = 0; row < board.size(); row++) {
     for (int col = 0; col < board.size(); col++) {
-      char number = board[row][col];
-
-      bool emptySquare = number == '.';
-      if (emptySquare) continue;
-
-      int box = (row / 3) * 3 + (col / 3);
-      if (seenInRow[row].count(number) || seenInCol[col].count(number) || seenInBox[box].count(number)) {
+      if (!validator.validSquare(row, col)) {
         return false;
       }
-
-      seenInRow[row].insert(number);
-      seenInCol[col].insert(number);
-      seenInBox[box].insert(number);
     }
   }
 
