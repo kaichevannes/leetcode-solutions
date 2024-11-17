ca1a2825c99ee67d12843d9d227a0dcd7f27857e
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-17 12:41:11 +0000

    build: redo build structure with explicit naming to avoid having to cmake each time a new file is added
    This is best practice for large projects with multiple developers. Technically
    you can get around it by doing touch filename but this will make the build fail
    on other peoples computers without giving them a useful error message.
    Explicitly adding files seems slow but is fast in the future.

---
+++ src/source_files.cmake
@@ -0,0 +1,19 @@
+set(ARRAY_STRING_SOURCE_FILES
+  array_string/01_merge_sorted_array.cc
+)
+
+set(HASHMAP_SOURCE_FILES
+  hashmap/01_ransom_note.cc
+)
+
+set(MATRIX_SOURCE_FILES
+  matrix/01_valid_sudoku.cc
+)
+
+set(SLIDING_WINDOW_SOURCE_FILES
+  sliding_window/01_minimum_size_subarray_sum.cc
+)
+
+set(TWO_POINTERS_SOURCE_FILES
+  two_pointers/01_valid_palindrome.cc
+)
---
+++ tests/test_files.cmake
@@ -0,0 +1,19 @@
+set(ARRAY_STRING_TEST_FILES
+  array_string/01_merge_sorted_array_test.cc
+)
+
+set(HASHMAP_TEST_FILES
+  hashmap/01_ransom_note_test.cc
+)
+
+set(MATRIX_TEST_FILES
+  matrix/01_valid_sudoku_test.cc
+)
+
+set(SLIDING_WINDOW_TEST_FILES
+  sliding_window/01_minimum_size_subarray_sum_test.cc
+)
+
+set(TWO_POINTERS_TEST_FILES
+  two_pointers/01_valid_palindrome_test.cc
+)
