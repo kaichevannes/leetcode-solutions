4146894ffe02372be1964237d047bcc77fd96164
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-20 17:32:01 +0000

    feat: add a bullet proof non-palindrome generator (at this point my test suite is significantly more complex than my SUT, I'm not sure if it's worth it or not because what if the generators aren't actually producing the correct output, and what more if the requirements change and now someone has to change this monstrous generator without really knowing what it's doing. It feels like a recipe for future wasted time if put into practice in a real production environment.)

--- tests/two_pointers/01_valid_palindrome_test.cc
+++ tests/two_pointers/01_valid_palindrome_test.cc
@@ -1,5 +1,8 @@
 #include "../../src/two_pointers/01_valid_palindrome.h"
+#include "rapidcheck/gen/Arbitrary.h"
+#include "rapidcheck/gen/Transform.h"
 #include <gtest/gtest.h>
+#include <random>
 #include <rapidcheck.h>
 #include <rapidcheck/gtest.h>
 
@@ -58,8 +61,88 @@ TEST_F(ValidPalindromeTest, FourLengthHalfPalindrome) {
   EXPECT_FALSE(validPalindrome.isPalindrome("abca"));
 }
 
+rc::Gen<std::string> genEvenPalindrome() {
+  return rc::gen::map<std::string>([](std::string firstHalf) {
+    std::string secondHalf(firstHalf.rbegin(), firstHalf.rend());
+    return firstHalf + secondHalf;
+  });
+}
+
+rc::Gen<std::string> genOddPalindrome() {
+  std::string middleLetter = {*rc::gen::character<char>()};
+  return rc::gen::map(genEvenPalindrome(),
+                      [middleLetter](std::string palindrome) {
+                        int middleIndex = palindrome.size() / 2;
+                        palindrome.insert(middleIndex, middleLetter);
+                        return palindrome;
+                      });
+}
+
+rc::Gen<std::string> genNonPalindrome() {
+  rc::Gen<char> genAlphaNumChar = rc::gen::suchThat(
+      rc::gen::character<char>(), [](char c) { return std::isalnum(c); });
+  rc::Gen<std::string> genAlphaNumString =
+      rc::gen::container<std::string>(genAlphaNumChar);
+  rc::Gen<std::string> genNonPalindromeAlphaNumString =
+      rc::gen::withSize([=](int size) {
+        if (size < 2) {
+          size = 2;
+        }
+        return rc::gen::mapcat(
+            rc::gen::container<std::string>(size, genAlphaNumChar),
+            [](std::string str) {
+              return rc::gen::map(
+                  rc::gen::inRange<int>(0, str.size() / 2), [str](int index) {
+                    std::string nonPalindrome = str;
+                    int indexToChange = str.size() - index - 1;
+                    if (!std::isalnum(nonPalindrome[index] + 1)) {
+                      nonPalindrome[indexToChange] = 'a';
+                    } else {
+                      nonPalindrome[indexToChange] = nonPalindrome[index] + 1;
+                    }
+                    return nonPalindrome;
+                  });
+            });
+      });
+
+  rc::Gen<char> genSpecialChar =
+      rc::gen::suchThat(rc::gen::inRange<char>(33, 127),
+                        [](char c) { return c != '\\' && !std::isalnum(c); });
+  rc::Gen<std::string> genSpecialString =
+      rc::gen::container<std::string>(genSpecialChar);
+
+  return rc::gen::mapcat(
+      genNonPalindromeAlphaNumString, [=](std::string palindrome) {
+        return rc::gen::map(genSpecialString, [palindrome](
+                                                  std::string specialChars) {
+          std::string finalPalindrome = palindrome;
+          for (char specialChar : specialChars) {
+            std::random_device rd;
+            std::mt19937 rng(rd());
+            std::uniform_int_distribution<int> dist(0, palindrome.size() - 1);
+            finalPalindrome.insert(finalPalindrome.begin() + dist(rng),
+                                   specialChar);
+          }
+          return finalPalindrome;
+        });
+      });
+}
+
 RC_GTEST_FIXTURE_PROP(ValidPalindromeTest, ReverseStringGivesSameValue, ()) {
   std::string input = *rc::gen::string<std::string>();
   std::string reverseInput(input.rbegin(), input.rend());
-  RC_ASSERT(validPalindrome.isPalindrome(input) == validPalindrome.isPalindrome(reverseInput));
+  RC_ASSERT(validPalindrome.isPalindrome(input) ==
+            validPalindrome.isPalindrome(reverseInput));
+}
+
+RC_GTEST_FIXTURE_PROP(ValidPalindromeTest, EvenPalindromes, ()) {
+  RC_ASSERT(validPalindrome.isPalindrome(*genEvenPalindrome()));
+}
+
+RC_GTEST_FIXTURE_PROP(ValidPalindromeTest, OddPalindromes, ()) {
+  RC_ASSERT(validPalindrome.isPalindrome(*genOddPalindrome()));
+}
+
+RC_GTEST_FIXTURE_PROP(ValidPalindromeTest, NonPalindromes, ()) {
+  RC_ASSERT_FALSE(validPalindrome.isPalindrome(*genNonPalindrome()));
 }
