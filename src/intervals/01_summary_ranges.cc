6162169f8fabfd3147086441b5257cf88ae4819c
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-22 17:30:48 +0000

    refactor: extract function

--- src/intervals/01_summary_ranges.cc
+++ src/intervals/01_summary_ranges.cc
@@ -6,21 +6,29 @@
 struct Range {
   int start;
   int end;
+
+  static Range from(int num) { return Range{num, num}; }
 };
 
-std::vector<std::string> SummaryRanges::summaryRanges(std::vector<int> &nums) {
+std::vector<Range> buildRanges(std::vector<int> &nums) {
   std::vector<Range> ranges;
 
-  auto inLatestRange = [&](int num) { return num != (ranges.back().end + 1); };
+  auto extendsLatestRange = [&](int num) {
+    return !ranges.empty() && num == ranges.back().end + 1;
+  };
 
   for (int num : nums) {
-    if (ranges.empty() || inLatestRange(num)) {
-      ranges.push_back(Range{num, num});
-    } else {
+    if (extendsLatestRange(num)) {
       ranges.back().end += 1;
+    } else {
+      ranges.push_back(Range::from(num));
     }
   }
 
+  return ranges;
+}
+
+std::vector<std::string> convertRangesToStrings(std::vector<Range> &ranges) {
   auto rangeToString = [](Range range) {
     if (range.start == range.end) {
       return std::format("{}", range.start);
@@ -31,4 +39,11 @@ std::vector<std::string> SummaryRanges::summaryRanges(std::vector<int> &nums) {
   std::vector<std::string> result;
   std::ranges::transform(ranges, std::back_inserter(result), rangeToString);
   return result;
+}
+
+std::vector<std::string> SummaryRanges::summaryRanges(std::vector<int> &nums) {
+  if (nums.empty())
+    return {};
+  std::vector<Range> ranges = buildRanges(nums);
+  return convertRangesToStrings(ranges);
 }
