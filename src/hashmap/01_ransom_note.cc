dd3169de560eefc1c4f4f0c5c6878edc5c30d1a9
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-22 13:49:08 +0000

    feat: working ransom note solution using string find and erase

--- src/hashmap/01_ransom_note.cc
+++ src/hashmap/01_ransom_note.cc
@@ -1,8 +1,9 @@
 #include "01_ransom_note.h"
-#include <algorithm>
 
 bool RansomNote::canConstruct(std::string ransomNote, std::string magazine) {
-  std::sort(ransomNote.begin(), ransomNote.end());
-  std::sort(magazine.begin(), magazine.end());
-  return (magazine.find(ransomNote) != std::string::npos);
+  for (char c : ransomNote) {
+    if (magazine.find(c) == std::string::npos) return false;
+    magazine.erase(magazine.find(c), 1);
+  }
+  return true;
 }
