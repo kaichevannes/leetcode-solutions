42d415e4013793a264abba366d33d6e579c568d8
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-23 19:56:36 +0000

    refactor: substitute algorithm (floyd tortoise and hare cycle detection)

--- src/linked_list/01_linked_list_cycle.cc
+++ src/linked_list/01_linked_list_cycle.cc
@@ -1,14 +1,13 @@
 #include "01_linked_list_cycle.h"
-#include <unordered_set>
 
 bool LinkedListCycle::hasCycle(ListNode *head) {
-  std::unordered_set<ListNode*> seen;
-  ListNode* current = head;
+  ListNode* slow = head;
+  ListNode* fast = head;
 
-  while (current != NULL) {
-    if (seen.count(current)) return true;
-    seen.insert(current);
-    current = current->next;
+  while (fast != NULL && fast->next != NULL) {
+    slow = slow->next;
+    fast = fast->next->next;
+    if (slow == fast) return true;
   }
   return false;
 }
