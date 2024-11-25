ec108823894c22593c450c5e93c8ac05915b2cdb
Kai Chevannes <chevannes.kai@gmail.com>
2024-11-25 18:46:22 +0000

    test: generate terminating and cyclic lists

--- tests/linked_list/01_linked_list_cycle_test.cc
+++ tests/linked_list/01_linked_list_cycle_test.cc
@@ -1,10 +1,16 @@
 #include "linked_list/01_linked_list_cycle.h"
+#include "rapidcheck/gen/Numeric.h"
 #include <gtest/gtest.h>
 #include <rapidcheck.h>
 #include <rapidcheck/gtest.h>
 
-void PrintTo(const ListNode* node, std::ostream* os) {
-  *os << "Node(" << node->val << ")";
+std::ostream &operator<<(std::ostream &os, const ListNode *node) {
+  if (node == NULL) {
+    os << "NULL";
+    return os;
+  }
+  os << "Node(" << node->val << ") -> " << node->next;
+  return os;
 }
 
 class LinkedListCycleTest : public testing::Test {
@@ -26,24 +32,49 @@ TEST_F(LinkedListCycleTest, TrueForSmallestCycle) {
 }
 
 namespace rc {
-
 class LinkedListCycleTestProperty : public LinkedListCycleTest {
 protected:
-  Gen<ListNode> genEndNode() {
-    return gen::construct<ListNode>(gen::arbitrary<int>());
+  Gen<ListNode *> genEndNode() {
+    return gen::map(gen::arbitrary<int>(),
+                    [](int val) { return new ListNode(val); });
   }
 
-  Gen<ListNode> genTerminatingLinkedList() {
-    return gen::mapcat(genEndNode(), [this](ListNode node) {
-      return gen::weightedOneOf<ListNode>(
-          {{8, genTerminatingLinkedList()}, {2, gen::just(node)}});
+  Gen<ListNode *> genTerminatingLinkedList() {
+    return gen::apply(
+        [](ListNode *node, ListNode *next) {
+          node->next = next;
+          return node;
+        },
+        genEndNode(),
+        gen::weightedOneOf<ListNode *>(
+            {{8, gen::lazy([this] { return genTerminatingLinkedList(); })},
+             {2, gen::just(static_cast<ListNode *>(NULL))}}));
+  }
+
+  Gen<ListNode *> genCyclicLinkedList() {
+    return gen::mapcat(genTerminatingLinkedList(), [](ListNode* head){
+      ListNode *current = head;
+      std::vector<ListNode*> nodes;
+      while (current != NULL) {
+        nodes.push_back(current);
+        current = current->next;
+      }
+      return gen::map(gen::inRange<int>(0, nodes.size()), [nodes](int target){
+        nodes.back()->next = nodes[target];
+        return nodes.front();
+      });
     });
   }
 };
 
-RC_GTEST_FIXTURE_PROP(LinkedListCycleTestProperty, Test, ()) {
-  ListNode head = *genTerminatingLinkedList();
-  RC_ASSERT(linkedListCycle.hasCycle(&head));
+RC_GTEST_FIXTURE_PROP(LinkedListCycleTestProperty, FalseInTermiantingLinkedList, ()) {
+  ListNode *head = *genTerminatingLinkedList();
+  RC_ASSERT_FALSE(linkedListCycle.hasCycle(head));
+}
+
+RC_GTEST_FIXTURE_PROP(LinkedListCycleTestProperty, TrueWhenListHasCycle, ()) {
+  ListNode *head = *genCyclicLinkedList();
+  RC_ASSERT(linkedListCycle.hasCycle(head));
 }
 
 } // namespace rc
