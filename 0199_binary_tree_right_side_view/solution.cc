c8ec636c115e7c33d92624f714330f9b13a25d31
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-14 19:01:39 +0000

    feat: 0199 initial solution

--- 0199_binary_tree_right_side_view/solution.cc
+++ 0199_binary_tree_right_side_view/solution.cc
@@ -1,23 +1,32 @@
 #include "solution.h"
-
-void recurse(TreeNode *node, std::vector<int> &result) {
-  // base
-  if (node == nullptr) {
-    return;
-  }
-
-  // pre
-  result.push_back(node->val);
-
-  // recurse
-  recurse(node->right, result);
-  recurse(node->left, result);
-
-  // post
-}
+#include <deque>
 
 std::vector<int> Solution::rightSideView(TreeNode *root) {
   std::vector<int> result = std::vector<int>{};
-  recurse(root, result);
+  std::deque<TreeNode *> queue;
+  std::deque<TreeNode *> nextLayer;
+
+  if (root) {
+    queue.push_back(root);
+  }
+
+  while (!queue.empty()) {
+    TreeNode *node = queue.front();
+    queue.pop_front();
+
+    if (node->left) {
+      nextLayer.push_back(node->left);
+    }
+    if (node->right) {
+      nextLayer.push_back(node->right);
+    }
+
+    if (queue.empty()) {
+      result.push_back(node->val);
+      queue.insert(queue.end(), nextLayer.begin(), nextLayer.end());
+      nextLayer.clear();
+    }
+  };
+
   return result;
 }
